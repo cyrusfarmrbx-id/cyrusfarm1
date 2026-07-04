f7e7d359276ebeb4ec899b029aa2761fcfcd0b99
cyrusfarmrbx-id <cyrusfarmrbx@gmail.com>
2026-07-04 21:27:57 +0700

    cyrusautoplaza1.lua

--- cyrusautoplaza1.lua
+++ cyrusautoplaza1.lua
@@ -524,7 +524,7 @@ local BLACKLIST_IDS = {
 }
 
 local AutoSetConfig = {
-    [228] = { Name = "Lochness Monster", Price = 51 },
+    [228] = { Name = "Lochness Monster", Price = 47 },
 }
 
 -- ================= LOAD LIBRARY & SYSTEM =================
