60a59e7b05ece0c03d2fb8f9ed83788f3027f12e
cyrusfarmrbx-id <cyrusfarmrbx@gmail.com>
2026-09-08 01:53:02 +0700

    cyrusautoplaza3.lua

--- cyrusautoplaza3.lua
+++ cyrusautoplaza3.lua
@@ -450,7 +450,7 @@ local AutoSetConfig = {
     [269] = { Name = "Elshark Gran Maja", Price = 6 },
     [145] = { Name = "Worm Fish", Price = 4 },
     [661] = { Name = "Elpirate Gran Maja", Price = 16 },
-    [243] = { Name = "Ruby", Price = 91 },
+    [243] = { Name = "Ruby", Price = 81 },
     [226] = { Name = "Megalodon", Price = 223 },
     [228] = { Name = "Lochness Monster", Price = 711 },
     [833] = { Name = "Bonemaw Tyrant", Price = 6 },
