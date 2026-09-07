f8d1b60f2154ea93cb1f8dcdd602f1ea2b36b259
cyrusfarmrbx-id <cyrusfarmrbx@gmail.com>
2026-09-08 01:52:25 +0700

    cyrusautoplaza2.lua

--- cyrusautoplaza2.lua
+++ cyrusautoplaza2.lua
@@ -443,7 +443,7 @@ local BLACKLIST_IDS = {
 local AutoSetConfig = {
     [145] = { Name = "Worm Fish", Price = 4 },
     [661] = { Name = "Elpirate Gran Maja", Price = 16 },
-    [243] = { Name = "Ruby", Price = 91 },
+    [243] = { Name = "Ruby", Price = 81 },
     [226] = { Name = "Megalodon", Price = 223 },
     [228] = { Name = "Lochness Monster", Price = 102 },
     [833] = { Name = "Bonemaw Tyrant", Price = 3 },
